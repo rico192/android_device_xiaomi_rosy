f5efecc20f461c94eab9d762eebaa410eab1b11a
rico192 <49857555+rico192@users.noreply.github.com>
2020-04-10 20:02:01 +0700

    Bring_colt

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -19,26 +19,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
+# Inherit some common Colt stuff.
+$(call inherit-product, vendor/colt/config/common.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_rosy
+PRODUCT_NAME := colt_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Bootanimation
-TARGET_BOOTANIMATION_NAME := 720
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOT_ANIMATION_RES := 720x1440
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
